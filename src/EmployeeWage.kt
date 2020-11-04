4cb0d0815fd4f46fc56b3d4508fc156ec62492ce
“Aniket_D” <aniketdakhare@gmail.com>
2020-11-04 12:03:45 +0530

    ADDED Code to calculate employee wage for part time employee.

--- src/EmployeeWage.kt
+++ src/EmployeeWage.kt
@@ -1,20 +1,28 @@
-class EmployeeWage
+class EmployeeWageComputation
 {
     private val present = 1
+    private val partTime = 2
     private val empRatePerHour = 20
 
-    fun getAttendance()
+    private fun getWorkingHours(): Int
     {
-        var empHrs = 0
-        if ((0..1).random() == present)
-            empHrs = 8
-        val empWage = empHrs * empRatePerHour
+        return when ((0..2).random())
+        {
+            present -> 8
+            partTime -> 4
+            else -> 0
+        }
+    }
+
+    fun getEmployeeWage()
+    {
+        val empWage = getWorkingHours() * empRatePerHour
         println("Employee Wage Per Day = $empWage")
     }
 }
 
 fun main()
 {
-    val empWage = EmployeeWage()
-    empWage.getAttendance()
+    val empWage = EmployeeWageComputation()
+    empWage.getEmployeeWage()
 }
