fcf525bd7f2c211dfef0dfd9b4840091b0c5fd99
“Aniket_D” <aniketdakhare@gmail.com>
2020-11-05 10:15:28 +0530

    ADDED Code to save wages in CSV file.

--- src/employeeWageComputation/EmployeeWage.kt
+++ src/employeeWageComputation/EmployeeWage.kt
@@ -1,3 +1,7 @@
+package employeeWageComputation
+
+import java.io.File
+
 class EmployeeWageComputation
 {
     private val present = 1
@@ -6,24 +10,37 @@ class EmployeeWageComputation
     private val workingDays = 20
     private val workingHours = 100
 
+    private fun writeToCSVFile(dailyHours: MutableMap<String, Int>)
+    {
+        val csvHeader = "Day,Wage"
+        val file = File("EmployeeWageDetails.csv")
+        file.writeText(csvHeader)
+        dailyHours.forEach { (day, hours) -> file.appendText("\n$day,${hours * empRatePerHour}") }
+    }
+
     private fun getWorkingHours(): Int
     {
         var totalEmpHrs = 0
         var daysCount = 0
+        val dailyWorkingHours = mutableMapOf<String, Int>()
+
         while (daysCount < workingDays && totalEmpHrs < workingHours)
         {
             daysCount ++
-            totalEmpHrs += when ((0..2).random())
+            val dailyHours = when ((0..2).random())
             {
                 present -> 8
                 partTime -> 4
                 else -> 0
             }
+            dailyWorkingHours["Day: $daysCount"] = dailyHours
+            totalEmpHrs += dailyHours
         }
+        writeToCSVFile(dailyWorkingHours)
         return totalEmpHrs
     }
 
-    fun getEmployeeWage()
+    fun printEmployeeWage()
     {
         val empWage = getWorkingHours() * empRatePerHour
         println("Employee Wage Per Day = $empWage")
@@ -33,5 +50,5 @@ class EmployeeWageComputation
 fun main()
 {
     val empWage = EmployeeWageComputation()
-    empWage.getEmployeeWage()
+    empWage.printEmployeeWage()
 }
